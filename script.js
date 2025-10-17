65f03567c337606e2cbbbbc0f80f7c2a632b4093
andregarcia0412 <andregarcia0412@gmail.com>
2025-10-16 21:04:56 -0300

    fix: mobile version

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function smoothlyScrollTo(sectionId){
         block: 'start'
     })
 }
+
+async function getTranslations(){
+    const response = await fetch("translations.json")
+    const data = await response.json()
+}
+
+const langButton = document.getElementById("btn-lang")
+langButton.addEventListener("click", () => {
+    langButton.textContent = `${langButton.textContent == "PTBR" ? "EN" : "PTBR"}`
+})
